88c6a237affcbcf3ddfcb20b54895af831d56c1f
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-08-25 18:49:52 +0200

    build: align Java and Kotlin targets on JVM 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,11 @@ android {
         versionName = "0.1.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = true
@@ -26,6 +31,10 @@ android {
     }
 
     sourceSets["main"].assets.srcDir(layout.buildDirectory.dir("generated/artAssets"))
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
