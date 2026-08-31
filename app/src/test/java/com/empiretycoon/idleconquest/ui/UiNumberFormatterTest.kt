3cb1976378736e6799095a7ee867e8e284f90978
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-08-31 18:20:13 +0200

    test: cover UI number formatting

---
+++ app/src/test/java/com/empiretycoon/idleconquest/ui/UiNumberFormatterTest.kt
@@ -0,0 +1,35 @@
+package com.empiretycoon.idleconquest.ui
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class UiNumberFormatterTest {
+    @Test
+    fun compactFormatsBaseAndThresholdValues() {
+        assertEquals("0", UiNumberFormatter.compact(0.0))
+        assertEquals("999", UiNumberFormatter.compact(999.0))
+        assertEquals("1.00K", UiNumberFormatter.compact(1_000.0))
+        assertEquals("1.50K", UiNumberFormatter.compact(1_500.0))
+        assertEquals("1.00M", UiNumberFormatter.compact(1_000_000.0))
+        assertEquals("1.00B", UiNumberFormatter.compact(1_000_000_000.0))
+        assertEquals("1.00T", UiNumberFormatter.compact(1_000_000_000_000.0))
+    }
+
+    @Test
+    fun compactRoundsUsingUsDecimalSeparator() {
+        assertEquals("1.23K", UiNumberFormatter.compact(1_234.0))
+        assertEquals("12.35M", UiNumberFormatter.compact(12_345_678.0))
+    }
+
+    @Test
+    fun multiplierDropsDecimalsForWholeNumbers() {
+        assertEquals("1", UiNumberFormatter.multiplier(1.0))
+        assertEquals("32", UiNumberFormatter.multiplier(32.0))
+    }
+
+    @Test
+    fun multiplierKeepsTwoDecimalsForFractionalValues() {
+        assertEquals("1.50", UiNumberFormatter.multiplier(1.5))
+        assertEquals("1.75", UiNumberFormatter.multiplier(1.75))
+    }
+}
