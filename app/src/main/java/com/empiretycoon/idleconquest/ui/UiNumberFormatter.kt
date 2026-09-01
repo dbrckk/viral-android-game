3382dda817c967aa7fc8014aada26784dec76b9d
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-01 21:52:24 +0200

    feat: extend idle economy number suffixes

--- app/src/main/java/com/empiretycoon/idleconquest/ui/UiNumberFormatter.kt
+++ app/src/main/java/com/empiretycoon/idleconquest/ui/UiNumberFormatter.kt
@@ -4,6 +4,14 @@ import java.util.Locale
 
 object UiNumberFormatter {
     fun compact(value: Double): String = when {
+        value >= 1e36 -> String.format(Locale.US, "%.2e", value)
+        value >= 1e33 -> String.format(Locale.US, "%.2fDc", value / 1e33)
+        value >= 1e30 -> String.format(Locale.US, "%.2fNo", value / 1e30)
+        value >= 1e27 -> String.format(Locale.US, "%.2fOc", value / 1e27)
+        value >= 1e24 -> String.format(Locale.US, "%.2fSp", value / 1e24)
+        value >= 1e21 -> String.format(Locale.US, "%.2fSx", value / 1e21)
+        value >= 1e18 -> String.format(Locale.US, "%.2fQi", value / 1e18)
+        value >= 1e15 -> String.format(Locale.US, "%.2fQa", value / 1e15)
         value >= 1e12 -> String.format(Locale.US, "%.2fT", value / 1e12)
         value >= 1e9 -> String.format(Locale.US, "%.2fB", value / 1e9)
         value >= 1e6 -> String.format(Locale.US, "%.2fM", value / 1e6)
