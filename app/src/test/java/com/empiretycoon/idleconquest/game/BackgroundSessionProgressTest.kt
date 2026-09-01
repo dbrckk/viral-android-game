cc9a36de41df5bb0b39233ea8787950ee8aec997
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-01 21:55:54 +0200

    test: preserve earliest background pause time

--- app/src/test/java/com/empiretycoon/idleconquest/game/BackgroundSessionProgressTest.kt
+++ app/src/test/java/com/empiretycoon/idleconquest/game/BackgroundSessionProgressTest.kt
@@ -22,13 +22,28 @@ class BackgroundSessionProgressTest {
     }
 
     @Test
-    fun laterPauseReplacesEarlierPauseTimestamp() {
+    fun repeatedPausePreservesOriginalPauseTimestamp() {
         val tracker = BackgroundSessionProgress()
         tracker.markPaused(10_000L)
         tracker.markPaused(12_000L)
 
+        assertEquals(10_000L, tracker.saveTimestamp(13_000L))
         val result = tracker.consumeResume(15_000L, 2.0)
-        assertEquals(3L, result!!.seconds)
-        assertEquals(6.0, result.earnings, 0.0)
+        assertEquals(5L, result!!.seconds)
+        assertEquals(10.0, result.earnings, 0.0)
+    }
+
+    @Test
+    fun saveTimestampUsesPauseTimeWhileBackgrounded() {
+        val tracker = BackgroundSessionProgress()
+        tracker.markPaused(10_000L)
+
+        assertEquals(10_000L, tracker.saveTimestamp(50_000L))
+    }
+
+    @Test
+    fun saveTimestampUsesCurrentTimeWhenActive() {
+        val tracker = BackgroundSessionProgress()
+        assertEquals(50_000L, tracker.saveTimestamp(50_000L))
     }
 }
